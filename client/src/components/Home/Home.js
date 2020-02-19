7076879d68855fdba271a2382474c8e59438b4d2
Dennis Idowu <dennis_idowu@live.com>
2020-02-18 20:13:13 -0500

    fixed file structure as well as created Home component

---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Home extends React.Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <div>
+        <h1>This is the HomePage</h1>
+      </div>
+    );
+  }
+}
+
+export default Home;
